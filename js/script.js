98fe76e3bb46e9c8b5fb32bd5cd8e3478dcf354c
AlexandrMonakhov <a.monahov@mail.ru>
2017-10-14 22:56:07 +0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function show()
+{
+	var open = document.querySelector("#cab");
+	open.style.display = "block";
+}
+function hide()
+{
+	var open = document.querySelector("#cab");
+	open.style.display = "none";
+}
+
+function show1()
+{
+	var open1 = document.querySelector(".write_us");
+	open1.style.display = "block";
+}
+function hide1()
+{
+	var open1 = document.querySelector(".write_us");
+	open1.style.display = "none";
+}
+
